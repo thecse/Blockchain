159772d6dabe689406ec186515dab10e790a86e2
thecse <71066359+thecse@users.noreply.github.com>
2021-05-08 00:48:33 +0530

    Update data.cpp

--- Blockchain/data.cpp
+++ Blockchain/data.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<pair<long double, long double>>public_key;
 vector<vector<long double>>get_data(string voter_id, string name, string party){
-    rsa r(rand()%150);
+    rsa r(rand()%40);
     vector<string> data={voter_id, name, "votes", "for", party};
     vector<vector<long double>> encdata(data.size());
     for(int i=0;i<data.size();i++){
